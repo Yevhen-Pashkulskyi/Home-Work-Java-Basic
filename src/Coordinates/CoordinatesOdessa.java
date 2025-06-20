40c2cd289fa1e685974940754b4fe2b81f3e57b0
Pashkulskyi Yevhen <pashkulskyevhen@gmail.com>
2025-06-20 22:31:39 +0300

    update HW 1

--- src/Coordinates/CoordinatesOdessa.java
+++ src/Coordinates/CoordinatesOdessa.java
@@ -4,6 +4,7 @@ public class CoordinatesOdessa {
     public static void main(String[] args) {
         double longitude = 46.4702111;
         double latitude = 30.7306393;
-        System.out.println("longitude = " + longitude + ", latitude = " + latitude);
+        String city = "Odessa";
+        System.out.println("Город: " + city + "\nКоординаты: долгота = " + longitude + ", широта = " + latitude);
     }
 }
