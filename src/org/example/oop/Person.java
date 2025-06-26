27c25f4c24ffb31f1f67686bd298cac833b6427a
Pashkulskyi Yevhen <pashkulskyevhen@gmail.com>
2025-06-27 00:16:49 +0300

    HW-12 Person

---
+++ src/org/example/oop/Person.java
@@ -0,0 +1,20 @@
+package org.example.oop;
+
+public class Person {
+    private String name;
+    private String surname;
+    private String city;
+    private String phone;
+
+    public Person(String name, String surname, String city, String phone) {
+        this.name = name;
+        this.surname = surname;
+        this.city = city;
+        this.phone = phone;
+        System.out.println(reminder());
+    }
+
+    private String reminder() {
+        return "Позвонить гражданину " + name + " " + surname + " с города " + city + " можно по номеру " + phone;
+    }
+}
---
+++ src/org/example/oop/PersonDemo.java
@@ -0,0 +1,11 @@
+package org.example.oop;
+
+public class PersonDemo {
+    public static void main(String[] args) {
+        Person willSmith = new Person("Will", "Smith", "New-York", "111-238-1234");
+
+        Person jackieChan = new Person("Jackie", "Chan", "Shanghai", "222-238-1234");
+
+        Person sherlockHolmes = new Person("Sherlok", "Holmes", "London", "444-238-1234");
+    }
+}
