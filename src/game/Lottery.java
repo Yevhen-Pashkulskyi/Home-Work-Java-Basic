d1d3da6a0d35983f3526b3dc7d55c0d4daaf4588
Pashkulskyi Yevhen <pashkulskyevhen@gmail.com>
2025-06-26 19:06:55 +0300

    complect task for game "Lottery"

--- src/game/Lottery.java
+++ src/game/Lottery.java
@@ -5,38 +5,62 @@ import java.util.concurrent.ThreadLocalRandom;
 public class Lottery {
     public static void main(String[] args) {
         int sizeTicket = 7;
+
         int[] playerTicket = fillTicketWithNumber(sizeTicket);
+        System.out.println("Player Ticket:");
         printTicket(playerTicket);
+        playerTicket = sortedNumberTicket(playerTicket);
+        printTicket(playerTicket);
+
         int[] organizeTicket = fillTicketWithNumber(sizeTicket);
+        System.out.println("Organize Ticket:");
         printTicket(organizeTicket);
+        organizeTicket = sortedNumberTicket(organizeTicket);
+        printTicket(organizeTicket);
+
+        System.out.println("Количество совпадений: " + searchNumberToAirs(playerTicket, organizeTicket));
     }
 
     private static int[] fillTicketWithNumber(int sizeTicket) {
         int[] ticket = new int[sizeTicket];
-        int low = 0;
-        int high = 9;
+        int highNumber = 9;
         for (int i = 0; i < ticket.length; i++) {
-            ticket[i] = ThreadLocalRandom.current().nextInt(low, high + 1);
+            ticket[i] = ThreadLocalRandom.current().nextInt(highNumber + 1);
         }
         return ticket;
     }
 
+    // печатает масив в одну строку
     private static void printTicket(int[] ticket) {
         for (int i = 0; i < ticket.length; i++) {
             System.out.print(ticket[i] + " ");
         }
+        System.out.println();
     }
 
+    // сортирует масив от меньшего к большему
     private static int[] sortedNumberTicket(int[] ticket) {
-        int[] result = new int[ticket.length];
-        int min = ticket[0];
-        for (int i = 1; i < ticket.length; i++) {
-            if (ticket[i] < min) {
-                min = ticket[i];
-                result[i] = min;
+        System.out.println("Sorted:");
+        for (int i = 0; i < ticket.length - 1; i++) {
+            for (int j = 0; j < ticket.length - 1 - i; j++) {
+                if (ticket[j] > ticket[j + 1]) {
+                    int temp = ticket[j];
+                    ticket[j] = ticket[j + 1];
+                    ticket[j + 1] = temp;
+                }
             }
-            result[i] = ticket[i];
         }
-        return result;
+        return ticket;
+    }
+
+    // поиск одинаковых значений в масиве
+    private static int searchNumberToAirs(int[] firstArray, int[] secondArray) {
+        int counter = 0;
+        for (int i = 0; i < firstArray.length; i++) {
+            if (firstArray[i] == secondArray[i]) {
+                counter++;
+            }
+        }
+        return counter;
     }
 }
