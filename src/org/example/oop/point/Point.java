553c8ce4eaa8ddfa629b22342b89e83dfc6dd3c2
Pashkulskyi Yevhen <pashkulskyevhen@gmail.com>
2025-06-29 19:39:56 +0300

    HW-16 Point - complected

---
+++ src/org/example/oop/point/Point.java
@@ -0,0 +1,82 @@
+package org.example.oop.point;
+
+import java.util.Objects;
+
+import static java.lang.Math.*;
+
+public class Point implements Cloneable {
+    private int x;
+    private int y;
+
+    // конструктор по умолчанию
+    public Point() {
+    }
+
+    // конструткор копирования
+    public Point(Point p) {
+        this.x = p.x;
+        this.y = p.y;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    // функция расcтояние точки от плоскости
+    public double distancePoint(int x, int y) {
+        return sqrt(pow(this.x - x, 2) + pow(this.y - y, 2));
+    }
+
+    // функция расстояние между двумя точками
+    public static double distanceBetweenPoint(Point firstPoint, Point secondPoint) {
+        if (firstPoint == null || secondPoint == null) {
+            return Double.POSITIVE_INFINITY;
+        }
+        return firstPoint.distancePoint(secondPoint.x, secondPoint.y);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Point point = (Point) o;
+        return x == point.x && y == point.y;
+    }
+
+    @Override
+    public String toString() {
+        return "Point{" +
+                "x=" + x +
+                ", y=" + y +
+                '}';
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
+    }
+
+    @Override
+    protected Point clone() throws CloneNotSupportedException {
+        Point clone = (Point) super.clone();
+        clone.x = x;
+        clone.y = y;
+        return clone;
+    }
+}
---
+++ src/org/example/oop/point/PointRunner.java
@@ -0,0 +1,37 @@
+package org.example.oop.point;
+
+public class PointRunner {
+    public static void main(String[] args)throws CloneNotSupportedException {
+        Point firstPoint = new Point();
+        firstPoint.setX(10);
+        int xFirstPoint = firstPoint.getX();
+        firstPoint.setY(10);
+        int yFirstPoint = firstPoint.getY();
+        System.out.println(firstPoint);
+
+        Point secondPoint = new Point();
+        secondPoint.setX(5);
+        int xSecondPoint = secondPoint.getX();
+        secondPoint.setY(5);
+        int ySecondPoint = secondPoint.getY();
+        System.out.println(secondPoint);
+
+        double pointFirst = firstPoint.distancePoint(xFirstPoint, yFirstPoint);
+        System.out.println(pointFirst);
+        double pointSecond = secondPoint.distancePoint(xSecondPoint, ySecondPoint);
+        System.out.println(pointSecond);
+
+        double distance = Point.distanceBetweenPoint(firstPoint, secondPoint);
+        System.out.println("Растояние между точками: " + distance);
+
+        System.out.println("Равенство двух точек: " + firstPoint.equals(secondPoint));
+
+        Point copiedPoint = new Point(firstPoint);
+        System.out.println("Копированная точка: " + copiedPoint);
+        System.out.println("Равенство с первой точкой с копированной: " + firstPoint.equals(copiedPoint));
+
+        Point clonedPoint = firstPoint.clone();
+        System.out.println("Клонированная точка: " + clonedPoint);
+        System.out.println("Равенство с первой точки с клонированной: " + firstPoint.equals(clonedPoint));
+    }
+}
