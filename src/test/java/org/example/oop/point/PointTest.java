1d2a54617ebf2759680d57d59007c898199e8033
Yevhen <pashkulskyevhen@gmail.com>
2025-07-06 15:50:40 +0300

    Home Work 18 - Change project to maven

---
+++ src/test/java/org/example/oop/point/PointTest.java
@@ -0,0 +1,36 @@
+package org.example.oop.point;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PointTest {
+
+    @Test
+    void setX() {
+    }
+
+    @Test
+    void getX() {
+    }
+
+    @Test
+    void setY() {
+    }
+
+    @Test
+    void getY() {
+    }
+
+    @Test
+    void distancePoint() {
+    }
+
+    @Test
+    void distanceBetweenPoint() {
+    }
+
+    @Test
+    void testClone() {
+    }
+}
